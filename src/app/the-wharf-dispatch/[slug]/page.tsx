44e06e377b25e21329cc178a2aa12f5292fd14f1
euvini <viniciuseduardo@outlook.com>
2024-11-14 14:00:54 -0300

    Site updated

--- src/app/the-wharf-dispatch/[slug]/page.tsx
+++ src/app/the-wharf-dispatch/[slug]/page.tsx
@@ -5,10 +5,12 @@ import NavBar from "@/components/navbar";
 import Footer from "@/components/sections/footer";
 import NewsletterComponent from "@/components/sections/newsletter";
 import TextReveal from "@/components/textReveal";
+import { Card, CardContent } from "@/components/ui/card";
 import { useScrollByVh } from "@/hooks/useScrollByVh";
 import { useWindowWidth } from "@/hooks/useWindowWidth";
 import { cn, formatDate } from "@/lib/utils";
-import { getPostDetail } from "@/services/blog";
+import { getAllPosts, getPostDetail } from "@/services/blog";
+import { PostResponse } from "@/services/models/blog";
 import { BlogDetailsProps } from "@/services/models/blog-details";
 import { parse } from "date-fns";
 import { ArrowUpLeft } from "lucide-react";
@@ -22,7 +24,11 @@ export default function Article({ params }: { params: { slug: string } }) {
     const SCREEN_WIDTH = windowWidth
     const MOBILE_BREAKPOINT = 768
 
+    const heights = [332, 414, 284, 270, 284, 414, 332];
+    const mobileHeight = [319]
+
     const [data, setData] = useState<BlogDetailsProps | null>(null)
+    const [dataList, setDataList] = useState<PostResponse | null>(null)
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<string | null>(null)
 
@@ -45,6 +51,21 @@ export default function Article({ params }: { params: { slug: string } }) {
             setLoading(false)
         }
     }
+
+    async function handleDataList() {
+        try {
+            const response = await getAllPosts()
+            setDataList(response)
+        } catch (err) {
+            setError('Failed to fetch data')
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        handleDataList()
+    }, [])
 
     useEffect(() => {
         handleData()
@@ -200,6 +221,72 @@ export default function Article({ params }: { params: { slug: string } }) {
                             </section>
                         )
                     }
+
+                    <section className="flex flex-col w-full px-8 py-20 gap-8 text-navy items-center bg-sand max-w-screen-2xl">
+                        <Reveal>
+                            <>
+                                <div className="flex flex-col text-center gap-8 items-center justify-center max-w-full md:gap-16">
+
+                                    {
+                                        SCREEN_WIDTH > MOBILE_BREAKPOINT && (
+                                            <TextReveal text="Related posts" />
+                                        )
+                                    }
+
+                                    <h1 className={'header1MD leading-none md:hidden'}>
+                                        Related posts
+                                    </h1>
+
+                                </div>
+
+                                <section className="flex flex-wrap gap-16 md:justify-between items-center justify-center pb-20 max-w-6xl justify-self-center mt-20">
+                                    {
+                                        dataList && dataList.slice(0, 3).map((item, index) => {
+                                            const HEIGHT = SCREEN_WIDTH < MOBILE_BREAKPOINT ? mobileHeight[0] : heights[index % heights.length];
+
+                                            return (
+                                                <div key={index} className={cn("w-[341px]", "relative flex flex-col")}>
+                                                    <Card
+                                                        className={cn("w-[341px]", "flex flex-col items-center rounded-none justify-self-center cursor-pointer")}
+                                                        style={{ height: `${HEIGHT}px`, minHeight: mobileHeight[0] }} // Altura mínima para evitar sobreposição
+                                                    >
+                                                        <CardContent className="flex flex-col items-center w-full h-full p-0">
+                                                            <Link href={`/the-wharf-dispatch/${item.slug}`} className="relative h-full w-full">
+                                                                <div className="relative flex-grow w-full h-full max-h-[282px] md:max-h-[652px]">
+                                                                    <Image
+                                                                        src={item.acf_medias.section_1 ?? ''}
+                                                                        alt={'banner'}
+                                                                        width={304}
+                                                                        height={SCREEN_WIDTH < MOBILE_BREAKPOINT ? 292 : 414}
+                                                                        className="object-cover w-full h-full"
+                                                                    />
+                                                                    <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-black/0 hover:bg-marigold/80 transition-colors" />
+                                                                </div>
+                                                                <div className="w-full px-2 z-10" style={{ backgroundColor: item.acf.color }}>
+                                                                    <div className="flex justify-between items-center bg-navy p-2 w-full">
+                                                                        <span className="body2 text-white">{item.acf.type?.toUpperCase()}</span>
+                                                                    </div>
+                                                                </div>
+                                                            </Link>
+                                                        </CardContent>
+                                                    </Card>
+                                                    <span className="pb-2 pt-8 px-4 text-navy flex-wrap flex w-full subheader3-bold" style={{ marginTop: '16px' }}>
+                                                        {item.title.rendered}
+                                                    </span>
+                                                    <span className="py-2 px-4 text-navy flex-wrap flex w-full body2">
+                                                        {item.acf.section_1.description.length > 115
+                                                            ? `${item.acf.section_1.description.slice(0, 115)}...`
+                                                            : item.acf.section_1.description}
+                                                    </span>
+                                                    {/* <span className="py-2 px-4 text-navy flex-wrap flex w-full subheader5">{formatDate(date(item?.acf?.date ?? ''))}</span> */}
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </section>
+                            </>
+                        </Reveal>
+                    </section>
 
                     <NewsletterComponent />
                     <Footer />
